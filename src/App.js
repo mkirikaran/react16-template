25f2712595987a153e93b9ff363c8003be4c5a09
unknown <manikkavasagarkirikaran93@gmail.com>
2021-10-28 10:25:10 +0530

    initail commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import "./styles.css";
+
+
+const App = ({ deleteAll, persistTodos }) => {
+ 
+  return (
+    <div className="App">
+ <h1>hello world template</h1>
+    </div>
+  );
+};
+export default App
